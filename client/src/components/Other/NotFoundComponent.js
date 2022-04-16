42ac2c8ded65bd9624b5882c110a4dd687c913d5
niikoolaa04 <nikola04jovicic@gmail.com>
2022-04-16 21:23:47 +0200

    404 page dark theme & icon fix

--- client/src/components/Other/NotFoundComponent.js
+++ client/src/components/Other/NotFoundComponent.js
@@ -4,14 +4,14 @@ import NavComponent from '../Navigation/NavComponent';
 import FooterComponent from './FooterComponent';
 
 function NotFoundComponent() {
-  return <div>
+  return <div className='bg-darker'>
     <NavComponent active={"other"} />
     <div className='py-5'>
       <div className="container-fluid">
         <div className="row m-0 p-0">
           <div className='text-center mt-5'>
-            <img className='m-0 p-0' style={{ transform: "scale(1)" }} src="https://cdn-icons.flaticon.com/png/128/2797/premium/2797387.png?token=exp=1649519606~hmac=538a7a394dcdda1230fefe1ad13c6a4c" alt="" />
-            <p className="fs-5 pt-3 pb-2">Page requested couldn't be found.</p>
+            <img className='m-0 p-0' style={{ transform: "scale(1)" }} src="https://cdn-icons-png.flaticon.com/256/5058/5058040.png" alt="" />
+            <p className="text-light fs-3 pt-5 pb-2">Page requested couldn't be found.</p>
             <Link to={"/"}>
               <button type="button" class="btn btn-primary btn-lg">Return Home</button>
             </Link>
