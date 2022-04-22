8e96680c9ed564c9071ea6333d4433b11fd5e6f5
niikoolaa04 <nikola04jovicic@gmail.com>
2022-04-22 22:31:07 +0200

    mm

--- server/server.js
+++ server/server.js
@@ -28,21 +28,9 @@ mongoose.connect(process.env.SERVER_MONGO_URL, {
 }).then(() => console.log(`Successfully Connected to MongoDB.`))
   .catch((err) => console.log(err));
 
+app.get('/', (req, res) => { res.send('Hello from Express!') })
+
 app.use("/auth", auth);
 app.use("/api", api);
 app.use("/users", users);
-
-
-app.use(express.static("./build"))
-app.get("/", (req, res, next) => {
-  //res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-  res.sendFile(path.join(__dirname, './build/index.html'))
-})
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
 app.listen(process.env.SERVER_PORT, () => console.log(`Listenting on Port ${process.env.SERVER_PORT}.`));
