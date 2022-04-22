0855dbffebb992958659e6aa047892c10dcf74df
niikoolaa04 <nikola04jovicic@gmail.com>
2022-04-22 21:44:34 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -33,13 +33,13 @@ app.use("/api", api);
 app.use("/users", users);
 
 
-app.use(express.static("../client/build"))
+app.use(express.static("./build"))
 app.get("/", (req, res, next) => {
   //res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  res.sendFile(path.join(__dirname, './build/index.html'))
 })
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
